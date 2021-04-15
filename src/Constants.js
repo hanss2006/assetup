087902ad2b9004e301b1ee673fceb97ab65addb7
hanss2006 <hanss@live.ru>
2021-04-15 21:02:56 +0300

    Initial commit

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-export const JPA_API_URL = process.env.PUBLIC_URL+'/api'
-//export const JPA_API_URL = 'http://localhost:8080/assetup/api'
+//export const JPA_API_URL = process.env.PUBLIC_URL+'/api'
+export const JPA_API_URL = 'http://localhost:8080/assetup/api'
